05cae266f7597de12a5d8fd14aecb76d86d87634
SinPi310 <rebel.hero6@gmail.com>
2026-07-01 01:35:59 +0200

    Dodanie funkcji rate
    Dodałem funkcję rate i poprawiłem parę kroków muszę teraz jeszcze zrobić z edytowaniem i menu w terminalu i o najważniejszym zapomniałem musze dodać wykresy i analize danych

--- __init__.py
+++ __init__.py
@@ -2,36 +2,36 @@ import os
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from dotenv import load_dotenv
-from spotify_utilities import album_download, format_ms
+from spotify_utilities import album_download as download, open_csv_file as open, save_to_csv as save, rate_tracks as rate
+
+
 
 def main():
     load_dotenv()
-
     client_id = os.getenv("SPOTIPY_CLIENT_ID")
     client_secret = os.getenv("SPOTIPY_CLIENT_SECRET")
 
     if not client_id or not client_secret or client_id == 'TWOJ_CLIENT_ID':
-        print("!!!Błąd z kluczami API!!!")
+        print("!!!Valid API KEY!!!")
         return
 
     try:
-        # 1. LOGOWANIE
+        # 1. Token autoryzacyjny
         auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
         sp = spotipy.Spotify(auth_manager=auth_manager)
 
-        # 2. POBRANIE albumu i utworów
-        album_id = 'https://open.spotify.com/album/0rXkEiNBQAspPBy0ai23F9'
-        album_data = sp.album(album_id)
-        tracks_list = album_download(sp, album_id)
+        link = input("Pass your spotify album link: ").strip()
 
-        # 4. DISPLAY tytułu albumu
-        print(f"\n{album_data['name']}")
-        print(f"{album_data['release_date']}")
-        print()
+        album_data = sp.album(link)
+        album_name = album_data['name']
 
-        for track in tracks_list:
-            duration_formatted = format_ms(track['duration_ms'])
-            print(f" {track['track_number']}. {track['title']} [{duration_formatted}]")
+        row_album_data = download(sp, link)
+
+        rated_album = rate(row_album_data, album_name)
+
+        path = save(rated_album, album_name)
+
+        # open(r"D:\WSEI 2 year\SEM4\Python\Profiler\DB\samurai champloo music record departure.csv")
 
     except Exception as e:
         print(f"Wystąpił błąd: {e}")
--- spotify_utilities.py
+++ spotify_utilities.py
@@ -1,12 +1,15 @@
+import os
+import csv
 import spotipy
+import pandas as pd
+from datetime import datetime
 
 def album_download(sp: spotipy.Spotify, album_url: str) -> list:
     print("+---------------------------------------+")
-    print("|      Pobieram utwory z albumu...      |")
+    print("|       Downloading tracks...           |")
 
     result = sp.album_tracks(album_url)
     tracks = result['items']
-
     tracks_list = []
 
     while result['next']:
@@ -20,13 +23,67 @@ def album_download(sp: spotipy.Spotify, album_url: str) -> list:
             'artist': ', '.join([artist['name'] for artist in track['artists']]),
             'disc_number': track['disc_number'],
             'track_number': track['track_number'],
-            'duration_ms': track['duration_ms']
+            'duration_ms': track['duration_ms'],
+            'rate': 0,
+            'superstar': False,
+            'rate_date': ''
         }
         tracks_list.append(track_info)
 
-    print(f"|          Pobrano {len(tracks_list)} utworów!          |")
+    print(f"|         Downloaded {len(tracks_list)} tracks!         |")
     print("+---------------------------------------+")
     return tracks_list
+
+def save_to_csv(tracks_list: list, album_name: str) -> str:
+    os.makedirs("DB", exist_ok=True)
+    album_name_formate = "".join([c for c in album_name if c.isalnum() or c in " _-"]).rstrip()
+    path = os.path.join("DB", f"{album_name_formate}.csv")
+
+    df = pd.DataFrame(tracks_list)
+    df.to_csv(path, index=False, encoding='utf-8-sig')
+
+    return path
+
+def open_csv_file(file_path: str) -> list:
+    with open(file_path, encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+
+        file_name = os.path.splitext(os.path.basename(file_path))[0]
+        print(f"\n{file_name}")
+
+        for track in reader:
+            special = "*" if track['superstar'] else ""
+            print(f"{track['track_number']}. {track['title']} [{track['artist']}]   {track['rate']}{special}")
+
+def rate_tracks(track_list: list, album_name: str) -> list:
+    print("\nHi, \n pleace tipe rate, between 1 and 10, after the track name. \n PS. You can also tipe [ * ] for super ultra grate tracks")
+    print("")
+    print(album_name)
+
+    for track in track_list:
+        while True:
+            user_rate = input(f" {track['track_number']}. {track['title']} [{track['artist']}] -> rate: ").strip()
+
+            is_superstar = '*' in user_rate
+            user_rate_number = user_rate.replace('*', '')
+
+            try:
+                user_rate_int = int(user_rate_number)
+
+                if 1 <= user_rate_int <= 10:
+                    track['rate'] = user_rate_int
+                    track['superstar'] = is_superstar
+                    track['rate_date'] = datetime.now().strftime("%Y-%m-%d")
+                    break
+
+                else:
+                    print("Rate must be between 1 and 10!!!")
+
+            except ValueError:
+                print("Valid user input!!!")
+
+    return track_list
+
 
 def format_ms(ms: int) -> str:
     minutes = int((ms / (1000 * 60)) % 60)
